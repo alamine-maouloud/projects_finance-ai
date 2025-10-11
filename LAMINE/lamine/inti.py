6c09d543b7eb5bfcb80dbfd7dcf4447a1cb3ad21
Alamine1209 <maouloudalamines@outlook.fr>
2025-10-11 14:21:57 +0200

    Update inti.py

--- LAMINE/lamine/inti.py
+++ LAMINE/lamine/inti.py
@@ -1,1 +1,30 @@
+mkdir lamine && cd lamine
+python -m venv .venv
+###
 
+python3 -m venv .venv
+source .venv/bin/activate
+cd ~/lamine
+ls -la
+
+###
+
+mkdir -p scripts data tests lamine/core lamine/domains lamine/app (création des dossiers)
+###
+
+touch lamine/__init__.py lamine/core/__init__.py lamine/domains/__init__.py lamine/app/__init__.py (création des paquets vide)
+###
+
+(Création des fichiers txt et md) 
+
+cat > requirements.txt <<'TXT' 
+pandas>=2.1
+numpy>=1.26
+scikit-learn>=1.4
+streamlit>=1.36
+matplotlib>=3.8
+pyyaml>=6.0
+joblib>=1.3
+pytest>=8.0
+TXT
+###
